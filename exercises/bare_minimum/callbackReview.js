dee8b17cd8494a4f23835dc3abf20a7742d090c1
Kieran <kieran.yun@gmail.com>
2022-03-15 12:29:31 -0700

    finished pluckFirstLineFromFile

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,15 +4,44 @@
 
 var fs = require('fs');
 var request = require('needle');
+const readline = require('readline');
+const https = require('https');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(`${filePath}`, (err, fileData) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, fileData.toString().split('\n')[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+
+//Input - a URL
+//Output - I think we have to make a get request and then get the response's status code
+//
+var getStatusCode = function (url, callback) {
   // TODO
+  // let makeGetRequest = function(url, CB) {
+  //   return $.ajax({
+  //     url: url,
+  //     type: 'GET',
+  //     dataType: 'json',
+  //     success: CB
+  //   });
+  // }
+
+  // makeGetRequest(url, )
+  https.request(url, (res) => {
+    console.log(res.statusCode);
+  });
+  //if the response method is GET
+  //then get the response's status code
+
 };
 
 // Export these functions so we can test them and reuse them in later exercises
